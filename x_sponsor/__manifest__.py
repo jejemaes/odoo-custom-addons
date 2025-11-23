32a940831d8391400be26d6f4260082a2380dddf
jerome.maes <jerome.maes@famoco.com>
2025-11-23 15:22:41 +0100

    [IMP] x_sponsor: add action to generate contract for tagged partner

--- x_sponsor/__manifest__.py
+++ x_sponsor/__manifest__.py
@@ -14,17 +14,20 @@
         "models/sponsor_stage.xml",
         "models/sponsor_contract.xml",
         "models/sponsor_sponsor.xml",
+        # Actions
+        "actions/sponsor_contract_actions.xml",
         # UI
         "views/sponsor_payment_method_views.xml",
         "views/sponsor_stage_views.xml",
-        "views/sponsor_contract_views.xml",
         "views/sponsor_sponsor_views.xml",
+        "views/sponsor_contract_views.xml",
         "views/menus.xml",
         # Security
         "security/sponsor_security.xml",
         "security/ir_model_access.xml",
         # Data
         "data/sponsor_stage_data.xml",
+        "data/res_partner_category_data.xml",
     ],
     "demo": [],
     "license": "OPL-1",
